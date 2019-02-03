f837417effaa52bb86ba4ae0455f954873aa54e3
Max Bolshakov <bolmaxim@gmail.com>
2019-02-03 23:20:43 +0300

    Method in the class Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,35 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main (String[] args) {
-		hello("world");
-		hello("User");
-		hello("Max");
 
-		Square s = new Square(5);
-		System.out.println("S" + s.l + "=" + s.area());
-
-		Rectangle r = new Rectangle(4,6);
-		System.out.println("S" + r.a + "and" + r.b + "=" + r.area());
-
-		Point p1 = new Point();
-		p1.a = 2;
-		p1.b = 4;
-		Point p2 = new Point();
-		p2.a = 6;
-		p2.b = 8;
-		System.out.println("distance between p1 and p2= " + distance(p1,p2));
+		Point p1 = new Point(2,4);
+		Point p2 = new Point(6,8);
+		System.out.println("distance between p1 and p2 = " + p1.distance(p2));
 
 	}
-	public static void hello(String somebody) {
-
-		System.out.println("Hello, " + somebody + "!");
-		}
-
-	public static double distance(Point p1, Point p2) {
-		return Math.sqrt ((p2.a - p1.a)*(p2.a - p1.a) + (p2.b - p1.b)*(p2.b - p1.b));
-
-
-	}
-
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,5 +4,16 @@ public class Point {
     public double a;
     public double b;
 
+    public Point(double a, double b) {
 
+        this.a = a;
+        this.b = b;
+    }
+
+    public double distance(Point p2) {
+        return Math.sqrt ((this.a - p2.a)*(this.a - p2.a) + (this.b - p2.b)*(this.b - p2.b));
+
+
+    }
+    
 }
