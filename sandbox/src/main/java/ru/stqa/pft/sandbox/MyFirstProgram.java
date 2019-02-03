6e6335da280cde517eb15c3e5b74ec17c80f4115
Max Bolshakov <bolmaxim@gmail.com>
2019-02-03 22:28:37 +0300

    create class Point and function distance(Point p1, Point p2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,23 +7,31 @@ public class MyFirstProgram {
 		hello("User");
 		hello("Max");
 
-		double l = 5;
-		System.out.println("S" + l + "=" + area(l));
+		Square s = new Square(5);
+		System.out.println("S" + s.l + "=" + s.area());
 
-		double a =4;
-		double b =6;
-		System.out.println("S" + a + "and" + b + "=" + area(a, b));
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("S" + r.a + "and" + r.b + "=" + r.area());
+
+		Point p1 = new Point();
+		p1.a = 2;
+		p1.b = 4;
+		Point p2 = new Point();
+		p2.a = 6;
+		p2.b = 8;
+		System.out.println("distance between p1 and p2= " + distance(p1,p2));
+
 	}
 	public static void hello(String somebody) {
 
 		System.out.println("Hello, " + somebody + "!");
 		}
 
-	public static double area(double len) {
-		return len * len;
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt ((p2.a - p1.a)*(p2.a - p1.a) + (p2.b - p1.b)*(p2.b - p1.b));
+
+
 	}
 
-	public static double area(double a, double b) {
-		return a * b;
-	}
+
 }
