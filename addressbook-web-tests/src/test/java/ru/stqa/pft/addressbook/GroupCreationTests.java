97b5cedffe7c273975202805999e23f1bf42ad44
Max Bolshakov <bolmaxim@gmail.com>
2019-02-10 16:31:09 +0300

    Create appManager

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,18 +1,17 @@
 package ru.stqa.pft.addressbook;
 
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
 
-        gotoGroupPage();
-        initGroupCreation();
-        fillGroupForm(new GroupData("test1", "test2", "test3"));
-        submitGroupCreation();
-        returnToGroupPage();
+        app.gotoGroupPage();
+        app.initGroupCreation();
+        app.fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.submitGroupCreation();
+        app.returnToGroupPage();
     }
 
 }
