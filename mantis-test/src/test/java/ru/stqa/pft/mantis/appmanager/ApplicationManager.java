49e68ffda71c28149d4e5b4643f518c9d6300e4c
Max Bolshakov <bolmaxim@gmail.com>
2019-03-23 18:18:25 +0300

    Task 18 Change Password

--- mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,10 +1,11 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.apache.http.impl.client.HttpClients;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.safari.SafariDriver;
 
 import java.io.File;
 import java.io.FileReader;
@@ -13,38 +14,92 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-
-    private final Properties propeties;
-    public WebDriver wd;
+    private final Properties properties;
+    private WebDriver wd;
 
     private String browser;
+    private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
+    private MailHelper mailHelper;
+    private JamesHelper jamesHelper;
+    private ChangePasswordHelper changePasswordHelper;
+    private DbHelper dbHelper;
 
-    public ApplicationManager(String browser)  {
-        propeties = new Properties();
+    public ApplicationManager(String browser) {
         this.browser = browser;
-
+        properties = new Properties();
     }
 
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
-        propeties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
-
-
-        if (browser.equals(BrowserType.SAFARI)) {
-            wd = new SafariDriver();
-        } else if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get(propeties.getProperty("web.baseUrl"));
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        dbHelper = new DbHelper();
     }
 
-
+    public DbHelper db() {
+        return dbHelper;
+    }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+    public ChangePasswordHelper changePassword(){
+        if (changePasswordHelper == null) {
+            changePasswordHelper = new ChangePasswordHelper(this);
+        }
+        return changePasswordHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
+
+    public MailHelper mail() {
+        if (mailHelper == null) {
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
+    }
+
+    public JamesHelper james() {
+        if (jamesHelper == null) {
+            jamesHelper = new JamesHelper(this);
+        }
+        return jamesHelper;
+    }
 }
--- mantis-test/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-test/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.IOException;
+import java.io.File;
+
 public class TestBase {
 
     protected static final ApplicationManager app
@@ -13,12 +16,13 @@ public class TestBase {
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
     }
 
-    @AfterSuite (alwaysRun = true)
-    public void tearDown() {
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws IOException, IOException {
+        app.ftp().restore("config_inc.php.bak", "config_inc.php");
         app.stop();
-
     }
 
 }
